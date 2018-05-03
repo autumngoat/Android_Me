56f332369831d29f65de5bb11d3b20badd0dea06
autumngoat <wong.sung.jeh@gmail.com>
2018-05-03 14:31:46 -0400

    refactor: Create MasterListFragment class and set GridView
    Implement empty constructor.
    Override onCreateView
      Inflate fragment_master_list.xml fragment layout
      Get a reference to the GridView inside the fragment layout

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,30 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class MasterListFragment extends Fragment {
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the fragment
+     */
+    public MasterListFragment() {}
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the Android-Me fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        // Get a reference to the GridView in the fragment_master_list xml layout file
+        GridView gridView = (GridView) rootView.findViewById(R.id.master_list_grid_view);
+
+        return rootView;
+    }
+}
