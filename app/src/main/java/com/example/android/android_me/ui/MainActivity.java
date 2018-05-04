f15cf43e9a24e4983b0eb612aefa2f39023ca987
autumngoat <wong.sung.jeh@gmail.com>
2018-05-03 20:29:35 -0400

    refactor: Launch intent when button is clicked

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -19,6 +19,8 @@ package com.example.android.android_me.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -71,11 +73,17 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         b.putInt("bodyKey", mBodyIndex);
         b.putInt("legsKey", mLegsIndex);
 
-        Intent intent = new Intent(this, AndroidMeActivity.class);
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
         intent.putExtra("bodyParts", b);
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
-
+        // Completed (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent));
+            }
+        });
     }
 
 }
